3c59dcc5594f5bca8a536aeeef8405d720e8f1e4
Gilles Rasigade <gilles.rasigade@chauffeur-prive.com>
2016-05-10 22:36:46 +0200

    Add MapperMongoDb to the index file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 const Queue = require('./src/Queue/Queue');
 const QueueAmqp = require('./src/Queue/QueueAmqp');
+const MapperMongoDb = require('./src/Mapper/MapperMongoDb');
 const Validator = require('./src/Validator/Validator');
 
 module.exports = {
   Queue,
   QueueAmqp,
+  MapperMongoDb,
   Validator,
 };
